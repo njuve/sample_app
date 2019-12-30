e8dbcb8f3a792db8ff39f9692fbabaebdc490d4c
njuve <ale.nnn.r@gmail.com>
2019-12-30 18:00:15 +0000

    8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:passwaord])
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
       redirect_to user
     else
-    end
+      # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
+    end
   end
 
   def destroy
